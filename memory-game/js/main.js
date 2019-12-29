11e8c7c4d54ee52d663a0597efb0d1776c002ad2
Mac.Os.10 <macos10@Macintosh-2.home>
2019-12-29 18:20:42 -0500

    javascript basics

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,6 +1,17 @@
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
+cardsInPlay.push(cardTwo);
+console.log("User flipped king");
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.")
+	}
+}
