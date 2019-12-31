2226529770e9b8a39329de9357fa1d6042c580e3
Mac.Os.10 <macos10@Macintosh-2.home>
2019-12-31 15:48:51 -0500

    objects

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,7 +1,38 @@
 
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suite: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suite:"diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suite: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suite: "diamonds",
+	cardImage:"images/king-of-diamonds.png"
+}
+];
 const cardsInPlay = [];
 
+function flipCard(cardId) {
+console.log("User flipped " + cards[cardId].rank);
+cardsInPlay.push(cards[cardId].rank);
+console.log(cards[cardId].suite);
+console.log(cards[cardId].cardImage);
+checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
 
 function checkForMatch() {
 if (cardsInPlay.length === 2) {
@@ -12,12 +43,3 @@ if (cardsInPlay.length === 2) {
 	}
 }
 }
-
-function flipCard(cardId) {
-console.log("User flipped " + cards[cardId]);
-cardsInPlay.push(cards[cardId]);
-checkForMatch();
-}
-
-flipCard(0);
-flipCard(2);
