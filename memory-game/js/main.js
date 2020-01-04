79c25ccc690632f90c370ec567c7b2cd2ffae9f7
Mac.Os.10 <macos10@Macintosh-2.home>
2020-01-04 12:11:03 -0500

    complete

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -23,7 +23,9 @@ const cards = [
 ];
 const cardsInPlay = [];
 
-function flipCard(cardId) {
+function flipCard() {
+	var cardId = this.getAttribute('data-id');
+	this.setAttribute('src', cards[cardId].cardImage);
 console.log("User flipped " + cards[cardId].rank);
 cardsInPlay.push(cards[cardId].rank);
 console.log(cards[cardId].suite);
@@ -31,10 +33,9 @@ console.log(cards[cardId].cardImage);
 checkForMatch();
 }
 
-flipCard(0);
-flipCard(2);
 
 function checkForMatch() {
+	// this.setAttribute('src', cards[cardId].cardImage);
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!")
@@ -43,3 +44,16 @@ if (cardsInPlay.length === 2) {
 	}
 }
 }
+
+function createBoard() {
+	for (let i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
+
+
+createBoard();
